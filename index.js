9782f4673a7698581662f08e8397c2badad6a998
Eliza Vardanyan <evarda17@gmail.com>
2018-08-08 05:51:17 +0400

    First commit

---
+++ index.js
@@ -0,0 +1,80 @@
+const Joi = require('joi');
+const express = require('express');
+const app =express();
+
+app.use(express.json());
+
+const genres = [
+	{id: 1, name: 'Comedy'},
+	{id: 2, name: 'Drama'},
+	{id: 3, name: 'Melodrama'},
+
+];
+app.get('/',(req, res) =>{
+	res.send('Hello World');
+
+})
+app.get('/api/genres',(req, res) => {
+	res.send(genres);
+});
+
+app.post('/api/genres', (req,res) =>{
+	const {error} = validateCourse(req.body);
+	if(error) return res.status(400).send(error.details[0].message);
+
+	const genre = {
+		id: genres.length +1,
+		name: req.body.name,
+};
+	genres.push(genre);
+	res.send(genre);
+});
+
+
+
+
+app.get('/api/genres/:id', (req, res) =>{
+
+	const genre = genres.find(c => c.id === parseInt(req.params.id));
+	if (!genre) return res.status(404).send('ID not found');
+	res.send(genre);
+})
+const port =process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+/*app.post()
+app.put()
+app.delete()*/
+
+app.put('/api/genres/:id', (req,res) =>{
+	const genre = genres.find(c => c.id === parseInt(req.params.id));
+	if (!genre) return res.status(404).send('ID not found');
+
+	const {error} = validateCourse(req.body);
+	if ( error) return res.status(400).send(result.error.details[0].message);
+		
+
+	genre.name = req.body.name;
+	res.send(genre);
+
+});
+
+app.delete('/api/genres/:id', (req, res) =>{
+	const genre = genres.find(c => c.id === parseInt(req.params.id));
+	if (!genre) return res.status(404).send('ID not found');
+
+	//Delete
+	const index = genress.indexOf(genre);
+	genres.splice(index, 1);
+
+	res.send(genre);
+});
+
+
+
+
+function validateCourse(genre) {
+	const schema = {
+		name: Joi.string().min(3).required()
+	};
+	return Joi.validate(genre, schema);
+};
